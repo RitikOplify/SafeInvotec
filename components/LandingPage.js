b5a20665f69d1c4427af5a16851210892330bdb1
RitikOplify <ritik.ranjan@oplify.in>
2024-11-20 17:06:17 +0530

    height

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -43,64 +43,61 @@ const LandingPage = () => {
   }, []);
 
   return (
-   <div 
-   style={{
-    backgroundImage: `url(/HomePageBanner/homepageplainbackground.jpg)`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}
-   >
     <div
-   
-      className="max-w-[1440px] mx-auto overflow-hidden md:flex-row flex flex-col items-center"
+      style={{
+        backgroundImage: `url(/HomePageBanner/homepageplainbackground.jpg)`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className=" md:max-h-[75vh]"
     >
-      <div className="md:w-[47%] w-full z-10 bg-opacity-50 p-5 rounded-lg typewriter">
-        <div>
-          <h1
-            key={currentImage}
-            className="text-[#AA1F2E] lg:text-[34px] text-2xl w-fit font-bold overflow-hidden whitespace-nowrap animate-typing h-[45px]"
-          >
-            {slides[currentImage].title}
-          </h1>
-          <h2 className="text-black lg:py-1 text-2xl lg:text-[30px] font-extrabold">
-            {slides[currentImage].subtitle}
-          </h2>
-          <p className="lg:xl: sm:text-lg my-1">
-            {slides[currentImage].description}
-          </p>
-          <div className="flex lg:mt-5 mt-4 items-start gap-4 flex-wrap">
-            <div className="bg-[#AA1F2E] font-bold md:py-3 md:px-3 text-white py-2 px-2 text-xs rounded-lg">
-              VIEW OUR PRODUCTS
-            </div>
-            <div className="bg-black font-bold text-xs md:py-3 md:px-3 text-white py-2 px-2 rounded-lg">
-              LEARN MORE ABOUT
+      <div className="max-w-[1440px] mx-auto overflow-hidden md:flex-row flex flex-col items-center">
+        <div className="md:w-[47%] w-full z-10 bg-opacity-50 p-5 rounded-lg typewriter">
+          <div>
+            <h1
+              key={currentImage}
+              className="text-[#AA1F2E] lg:text-[34px] text-2xl w-fit font-bold overflow-hidden whitespace-nowrap animate-typing h-[45px]"
+            >
+              {slides[currentImage].title}
+            </h1>
+            <h2 className="text-black lg:py-1 text-2xl lg:text-[30px] font-extrabold">
+              {slides[currentImage].subtitle}
+            </h2>
+            <p className="lg:xl: sm:text-lg my-1">
+              {slides[currentImage].description}
+            </p>
+            <div className="flex lg:mt-5 mt-4 items-start gap-4 flex-wrap">
+              <div className="bg-[#AA1F2E] font-bold md:py-3 md:px-3 text-white py-2 px-2 text-xs rounded-lg">
+                VIEW OUR PRODUCTS
+              </div>
+              <div className="bg-black font-bold text-xs md:py-3 md:px-3 text-white py-2 px-2 rounded-lg">
+                LEARN MORE ABOUT
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="md:w-[53%] w-full h-full flex">
-
-        <div>
-          {slides.map(
-            (slide, index) =>
-              index === currentImage && (
-                <div
-                  key={index}
-                  className="flex items-center p-5 right-0 slide-in"
-                >
-                  <img
-                    src={slide.image}
-                    alt={slide.image || "Slide image"}
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-              )
-          )}
+        <div className="md:w-[53%] w-full h-full flex">
+          <div>
+            {slides.map(
+              (slide, index) =>
+                index === currentImage && (
+                  <div
+                    key={index}
+                    className="flex items-center p-5 right-0 slide-in"
+                  >
+                    <img
+                      src={slide.image}
+                      alt={slide.image || "Slide image"}
+                      className="object-cover w-full h-full"
+                    />
+                  </div>
+                )
+            )}
+          </div>
         </div>
       </div>
     </div>
-   </div>
   );
 };
 
