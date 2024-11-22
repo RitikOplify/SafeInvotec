d122380f102eb11e82ece0ef00fd50e54c95f065
RitikOplify <ritik.ranjan@oplify.in>
2024-11-22 14:19:58 +0530

    product page

---
+++ app/brazzing/[id]/page.js
@@ -0,0 +1,16 @@
+import BrazzingDetails from "@/components/BrazzingDetails";
+import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <Nav />
+      <BrazzingDetails />
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
